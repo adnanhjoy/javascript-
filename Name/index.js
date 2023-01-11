41904f58b5f5388da4cda5761db3305288a80fc9
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-11 20:27:32 +0600

    javascript file

---
+++ Name/index.js
@@ -0,0 +1,7 @@
+
+var fristname = "<h1>Adnan</h1>";
+var lastname = " <h2>Hossain</h2>";
+var nickname = " <h3>Joy<h3>"
+var fullname = fristname + lastname;
+
+document.write(fullname + nickname);
---
+++ Number Methods/index.js
@@ -0,0 +1,14 @@
+var num = 2.5678;
+
+// convert int to string
+// num = toString(num);
+
+// convert strig to int
+// num = parseInt(num);
+
+// convert strig to int
+
+// num = parseFloat(num);
+
+// console.log(num.toFixed(3))
+// console.log(num.toPrecision(3))
