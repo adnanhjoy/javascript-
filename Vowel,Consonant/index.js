a060799cebff991d06c2543184c0f4b733f0f2c2
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-12 15:56:44 +0600

    Result file edited new folder created

---
+++ Vowel,Consonant/index.js
@@ -0,0 +1,7 @@
+var letter = prompt("Enter Your letter: ");
+
+if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+document.write("Vowel");
+
+else
+document.write("Consonant");
--- result/index.js
+++ result/index.js
@@ -1,22 +1,22 @@
-var mark = prompt("Enter Your Mark = ");
+var mark = prompt("Enter your mark : ");
+        
+        if(mark >= 80 && mark <= 100)
+        document.write("Congratulations ! Your are passed." + ("</br>") + "Your letter grade is A+");
 
-if (mark >= 80)
-document.write("A+");
+        else if(mark >= 70 && mark <= 79)
+        document.write("Congratulations ! Your are passed." + ("</br>") + "Your letter grade is A+");
 
-else if(mark >= 70)
-document.write("A");
+        else if(mark >= 60 && mark <= 69)
+        document.write("Congratulations ! Your are passed." + ("</br>") + "Your letter grade is A+");
 
-else if(mark >= 60)
-document.write("A-");
+        else if(mark >= 50 && mark <= 59)
+        document.write("Congratulations ! Your are passed." + ("</br>") + "Your letter grade is A+");
 
-else if(mark >= 50)
-document.write("B");
+        else if(mark >= 40 && mark <= 49)
+        document.write("Congratulations ! Your are passed." + ("</br>") + "Your letter grade is A+");
 
-else if(mark >= 40)
-document.write("C");
+        else if(mark >= 33 && mark <= 39)
+        document.write("Congratulations ! Your are passed." + ("</br>") + "Your letter grade is A+");
 
-else if(mark >= 33)
-document.write("D");
-
-else
-document.write("F");
+        else
+        document.write("Sorry ! your are fail.");
