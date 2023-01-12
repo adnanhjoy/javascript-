22bfb7b84467d1576860bab28b239766443658ed
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-12 14:59:05 +0600

    Resut and even odd folder up

---
+++ Even Odd/index.js
@@ -0,0 +1,6 @@
+var num = prompt("Enter Your Number = ");
+
+if(num % 2 == 0)
+document.write("Even");
+else
+document.write("Odd");
---
+++ result/index.js
@@ -0,0 +1,22 @@
+var mark = prompt("Enter Your Mark = ");
+
+if (mark >= 80)
+document.write("A+");
+
+else if(mark >= 70)
+document.write("A");
+
+else if(mark >= 60)
+document.write("A-");
+
+else if(mark >= 50)
+document.write("B");
+
+else if(mark >= 40)
+document.write("C");
+
+else if(mark >= 33)
+document.write("D");
+
+else
+document.write("F");
