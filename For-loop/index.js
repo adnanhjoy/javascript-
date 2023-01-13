192afdbf94f36d19ccaa687b27246fe438db7399
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-13 10:36:54 +0600

    Using for loop

---
+++ For-loop/index.js
@@ -0,0 +1,16 @@
+// Sum of 5 Value
+
+// var n = parseInt(prompt("Enter your value: "));
+// sum = 0;
+// for (i = 1; i <= n; i++){
+//     sum = sum + i;
+// }
+// document.write(sum);
+
+for(var i = 1; i<=10; i++){
+    var num1 = "5";
+    var num2 = parseInt(prompt("Enter Second Number: "));
+    var sum = num1 * num2;
+    
+    document.write(num1 + "*" + num2 + " =" + sum +("</br>"));
+}
