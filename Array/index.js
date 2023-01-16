ce08fc70652dae6462f2df69a0813a8432bd64c2
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-16 22:53:48 +0600

    Array

---
+++ Array/index.js
@@ -0,0 +1,19 @@
+var names = ["Adnan", " Joy", " Sohag", " Ashik", " Sumon"];
+names.push("Sihab");
+document.write(names[5] + "</br>"+"</br>");
+
+
+var names = Array();
+names[0] = "Adnan Hossain";
+names[1] = "Suhag Hossain";
+names[2] = "Sumon Sarker";
+names[3] = "Mehedi Hasan";
+names[4] = "Abdul Ahad";
+document.write(names[1] + "</br>"+"</br>");
+
+
+var country1 = ["Bangladesh", " India", " Pakistan"];
+var country2 = [" Nepal", " Bhutan", " China"];
+
+var country = country1.concat(country2);
+document.write(country);
