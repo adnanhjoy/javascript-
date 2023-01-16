55e337d3ed995caffaa9740a1d9b37d0fdda2b30
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-16 17:30:36 +0600

    Traditional Operator

---
+++ Traditional Function/index.js
@@ -0,0 +1,12 @@
+//creating fucntion
+
+function square(){
+    var num = Number(prompt("Enter Your Number: "));
+    var result = num * num;
+    document.write("The Result is: " +result + "</br>");
+}
+
+//calling function
+
+square();
+square();
