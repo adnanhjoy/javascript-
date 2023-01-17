8109a3f04982d7d5f8346c3b7111f4d990c47027
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-17 22:13:24 +0600

    Date Object

---
+++ Date Object/index.js
@@ -0,0 +1,23 @@
+var date = new Date();
+document.write(date + "</br>" + "</br>");
+
+var year = date.getFullYear();
+document.write(year + "</br>" + "</br>");
+
+var month = date.getMonth();
+document.write(month + "</br>" + "</br>");
+
+var day = date.getDay();
+document.write(day + "</br>" + "</br>");
+
+var currentdate = date.getDate();
+document.write(currentdate + "</br>" + "</br>");
+
+var houre = date.getHours();
+document.write(houre + "</br>" + "</br>");
+
+var minute = date.getMinutes();
+document.write(minute + "</br>" + "</br>");
+
+var time = date.getTime();
+document.write(time + "</br>" + "</br>");
