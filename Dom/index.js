47829fe4f3433edc8dc9845474db3f74e2201833
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-18 09:59:21 +0600

    Document Object Model

---
+++ Dom/index.js
@@ -0,0 +1,17 @@
+document.getElementById("heading1").innerHTML = ("My name is Adnan");
+
+document.getElementsByTagName("h1")[1].innerHTML = ("This is Tag");
+
+document.getElementsByClassName("heading2")[0].innerHTML = ("This is Class");
+
+document.querySelector("#heading4").innerHTML = ("Query ID");
+
+document.querySelector(".para").innerHTML = ("Query Class");
+
+document.querySelector("p").innerHTML = ("Query Tag");
+
+document.querySelector("ul li").innerHTML = ("joy");
+
+document.querySelector(".link a").innerHTML = ("India");
+
+document.querySelectorAll("p")[1].innerHTML = ("Rahim");
