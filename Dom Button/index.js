d0b07d815d4614bfdaeffd68f9ac5fd6ac93bc4a
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-18 10:29:25 +0600

    Dom Button

---
+++ Dom Button/index.js
@@ -0,0 +1,13 @@
+function button (){
+    var mybar = document.querySelector("#para1");
+    mybar.innerHTML = ("You Have Succesfully Submitted");
+}
+
+var imag = document.querySelector("img");
+function button1 (){
+    
+    imag.src ="image/hospital.jpg";
+}
+function button2 (){
+    imag.src ="image/Marison.jpg";
+}
