dd1aa52ecb9e0c2dc1aecd2a2cdca2e89e74a858
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-19 21:57:56 +0600

    Daily Task

---
+++ Daily Task/Task 10/index.js
@@ -0,0 +1,9 @@
+function hide(){
+    var hide = document.querySelector("p");
+    hide.classList.remove("para");
+}
+
+function unhide(){
+    var unhide = document.querySelector("p");
+    unhide.classList.add("para");
+}
