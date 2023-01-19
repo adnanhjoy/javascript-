8461a2bf6650b81d8b845b7d82840dead5c3a94e
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-19 15:08:57 +0600

    Dom Traversing

---
+++ Dom Traversing/index.js
@@ -0,0 +1,11 @@
+var downwards = document.querySelector(".traversing");
+
+downwards.children[0];
+
+var upwards = document.querySelector("li");
+
+upwards.parentElement;
+
+var sideways = document.querySelector("li");
+var adnan = sideways.nextElementSibling;
+adnan.nextElementSibling;
