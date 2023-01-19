e39bd7acad9772273f6eb8c5d3e12926eb6e3e72
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-19 17:30:31 +0600

    Changing CSS Style Dynamicaly

---
+++ Change Css Style Dynamicaly/index.js
@@ -0,0 +1,9 @@
+function seen(){
+var seen = document.querySelector("img");
+seen.classList.add("picture");
+}
+
+function unseen(){
+var unseen = document.querySelector("img");
+unseen.classList.remove("picture");
+}
