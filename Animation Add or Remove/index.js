a0337a3d4ef2805cbbfd319620fc0aa2d5aa79fb
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-21 23:57:56 +0600

    Audio Play

--- Animation Add or Remove/index.js
+++ Animation Add or Remove/index.js
@@ -3,5 +3,13 @@ for(i = 0; i < len; i++){
     document.querySelectorAll(".anim")[i].addEventListener("click", function(){
         var text = this.innerHTML;
         console.log(text);
+        PlayAnimation(text);
+
     })
+}
+
+function PlayAnimation(text){
+    switch (text){
+        case "Anim 1":
+    }
 }
