24f12983a0e6847411d1f956ab1d0fe976bf0e64
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-22 12:53:47 +0600

    Audio Play

--- Audio Play/index.js
+++ Audio Play/index.js
@@ -3,21 +3,26 @@ for(i = 0; i < len; i++){
     document.querySelectorAll(".button")[i].addEventListener("click", function(){
         var text = this.innerHTML;
         console.log(text);
+        PlayAudio(text);
         
-        switch (text){
-            case "Music":
-                var audio = new Audio("sound/music.mp3");
-                audio.play();
-                break;
-            case "Speech":
-                var audio = new Audio("sound/speech.mp3");
-                audio.play();
-                break;
-            case "Nashed":
-                var audio = new Audio("sound/nasheed.mp3");
-                audio.play();
-                break;
-        };
     });
     
 };
+
+function PlayAudio(text){
+    switch (text){
+        case "Music":
+            var audio = new Audio("sound/music.mp3");
+            audio.play();
+            break;
+        case "Speech":
+            var audio = new Audio("sound/speech.mp3");
+            audio.play();
+            break;
+        case "Nashed":
+            var audio = new Audio("sound/nasheed.mp3");
+            audio.play();
+            break;
+    };
+
+}
