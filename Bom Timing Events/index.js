72c29f04499e3bee2255a9e6e95bab16fa15ee2c
engadnanhossain <engr.adnanhossain@gmail.com>
2023-01-29 14:49:03 +0600

    BOM timing event

---
+++ Bom Timing Events/index.js
@@ -0,0 +1,17 @@
+let message = document.querySelector("p");
+document.querySelector("button").addEventListener("click", function(){
+    message.innerHTML = "user registration succesful";
+    setTimeout(() => {
+        message.innerHTML = "";
+    }, 2000);
+});
+
+document.querySelector("button").addEventListener("click", timeCount);
+function timeCount(){
+    let count = 1;
+    message.innerText = count;
+    setInterval(() => {
+        count++
+        message.innerText = count;
+    }, 2000)
+}
